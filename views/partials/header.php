86694c57cc32c248f482a724ef044d452648327e
mpsxdeveloper <98715891+mpsxdeveloper@users.noreply.github.com>
2024-02-02 15:30:38 -0400

    Create header.php

---
+++ views/partials/header.php
@@ -0,0 +1,8 @@
+<div class="col-12 text-center pt-3">
+    <h3><i class="fa-brands fa-vuejs fa-bounce" style="color: #41B883;"></i> Games4Learn</h3>
+</div>
+<div class="col-12 text-center pb-0">
+    <span class="fst-italic">
+        <i class="bi bi-person-vcard-fill"></i> Olá, <?= $_SESSION["nome"] ?>
+    </span>
+</div>
